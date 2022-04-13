abea2d875ecbf00099d2c9653fc5aabeb04d2cbf
Fernando PN <fernandopn@me.com>
2022-04-13 19:42:53 +0200

    fix bashscripts based on linter

--- tools/backup/ListSnapshots.sh
+++ tools/backup/ListSnapshots.sh
@@ -1,20 +1,28 @@
-pushd "$(dirname "$0")"
+pushd "$(dirname "$0")" || exit
+
 
 #Decrypt Credentials
-pushd keys
+pushd keys || exit
 sops -d google_key.sops.json > .google_key.json
-export GOOGLE_PROJECT_ID=$(sops -d secrets.sops.json | jq -r .private_project)
-export RESTIC_PASSWORD=$(sops -d secrets.sops.json | jq -r .private_password)
-export BUCKET=$(sops -d secrets.sops.json | jq -r .private_bucket)
-popd
+
+GOOGLE_PROJECT_ID=$(sops -d secrets.sops.json | jq -r .private_project)
+export GOOGLE_PROJECT_ID
+
+RESTIC_PASSWORD=$(sops -d secrets.sops.json | jq -r .private_password)
+export RESTIC_PASSWORD
+
+BUCKET=$(sops -d secrets.sops.json | jq -r .private_bucket)
+export BUCKET
+
+popd || exit
 
 export GOOGLE_APPLICATION_CREDENTIALS=${PWD}/keys/.google_key.json
 
-echo $RESTIC_PASSWORD | restic -r gs:${BUCKET}:cluster/restic/$1 snapshots
+echo "$RESTIC_PASSWORD" | restic -r "gs:${BUCKET}:cluster/restic/$1" snapshots
 
 #Get rid of the Credentials
-pushd keys
+pushd keys || exit
 rm .google_key.json
-popd
+popd || exit
 
-popd
+popd || exit
--- tools/backup/RestoreSnapshot.sh
+++ tools/backup/RestoreSnapshot.sh
@@ -1,20 +1,28 @@
-pushd "$(dirname "$0")"
+pushd "$(dirname "$0")" || exit
 
 #Decrypt Credentials
-pushd keys
+pushd keys || exit
 sops -d google_key.sops.json > .google_key.json
-export GOOGLE_PROJECT_ID=$(sops -d secrets.sops.json | jq -r .private_project)
-export RESTIC_PASSWORD=$(sops -d secrets.sops.json | jq -r .private_password)
-export BUCKET=$(sops -d secrets.sops.json | jq -r .private_bucket)
-popd
 
-export GOOGLE_APPLICATION_CREDENTIALS=${PWD}/keys/.google_key.json
+GOOGLE_PROJECT_ID=$(sops -d secrets.sops.json | jq -r .private_project)
+export GOOGLE_PROJECT_ID
 
-echo $RESTIC_PASSWORD | restic -r gs:${BUCKET}:cluster/restic/$1 restore $2 --target restored/
+RESTIC_PASSWORD=$(sops -d secrets.sops.json | jq -r .private_password)
+export RESTIC_PASSWORD
+
+BUCKET=$(sops -d secrets.sops.json | jq -r .private_bucket)
+export BUCKET
+
+popd || exit
+
+GOOGLE_APPLICATION_CREDENTIALS=${PWD}/keys/.google_key.json
+export GOOGLE_APPLICATION_CREDENTIALS
+
+echo "$RESTIC_PASSWORD" | restic -r "gs:${BUCKET}:cluster/restic/$1" restore "$2" --target restored/
 
 #Get rid of the Credentials
-pushd keys
+pushd keys || exit
 rm .google_key.json
-popd
+popd || exit
 
-popd
+popd || exit
